7bc42c4d2eb8547ffd2843949c1486077d39a119
Dalton Sergio Leonardo <dalton.leonardo@portoseguro.com.br>
2020-08-02 22:57:17 -0300

    Aula 4 URL_TOP localhost|heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -35,18 +35,20 @@ function CadastroCategoria() {
     }
 
     useEffect(() => {
-        if (window.location.href.includes('localhost')) {
-            const URL = 'http://localhost:8080/categorias';
-            fetch(URL)
-                .then(async (respostaDoServer) => {
-                    if (respostaDoServer.ok) {
-                        const resposta = await respostaDoServer.json();
-                        setCategorias(resposta);
-                        return;
-                    }
-                    throw new Error('Não foi possível pegar os dados');
-                })
-        }
+        const URL_TOP = window.location.href.includes('localhost') ?
+            'http://localhost:8080/categorias' :
+            'https://dslflix.herokuapp.com/categorias';
+
+        fetch(URL_TOP)
+            .then(async (respostaDoServer) => {
+                if (respostaDoServer.ok) {
+                    const resposta = await respostaDoServer.json();
+                    setCategorias(resposta);
+                    return;
+                }
+                throw new Error('Não foi possível pegar os dados');
+            })
+
     }, []);
 
     return (
